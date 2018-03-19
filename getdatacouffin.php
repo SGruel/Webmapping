ed67685d049913dd0f9ed834d9c69a4a3a581ced
SGruel <simeon.gruel@gmail.com>
2018-03-19 10:01:27 +0100

    array created need to convert to json

---
+++ getdatacouffin.php
@@ -0,0 +1,19 @@
+<?php
+
+$dbconn = pg_connect("host=localhost port=5432 dbname=dbcouffin password=postgres");
+if (!$dbconn){
+  echo "Erreur de connection\n";
+  exit;
+};
+$query= "SELECT * FROM couffin "
+$result= pg_query($dbconn,$query)
+if(!result){
+  echo "Erreur de requete";
+  exit;
+};
+$array=pg_fetch_all($result) 
+
+
+
+
+?>
